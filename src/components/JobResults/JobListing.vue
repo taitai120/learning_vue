931177c341f5a474bfaa352ac9eacad186feef78
taitai120 <phantuantai7293@gmail.com>
2023-04-13 11:23:49 +0700

    finish 19

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,8 +1,26 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <job-item v-for="job in jobs" :key="job.id" :job="job" />
+      <job-item v-for="job in displayJobs" :key="job.id" :job="job" />
     </ol>
+
+    <div class="mt-8 mx-auto">
+      <div class="flex flex-row flex-nowrap">
+        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+        <router-link
+          v-if="previousPage"
+          :to="{ name: 'JobResults', query: { page: previousPage } }"
+          class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >Previous</router-link
+        >
+        <router-link
+          v-if="nextPage"
+          :to="{ name: 'JobResults', query: { page: nextPage } }"
+          class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >Next</router-link
+        >
+      </div>
+    </div>
   </main>
 </template>
 
@@ -17,15 +35,40 @@ export default {
   data() {
     return {
       jobs: [],
+      limit: 10,
+      totalPages: null,
     };
   },
   mounted() {
     this.getData();
   },
+  computed: {
+    currentPage() {
+      const pageString = this.$route.query.page || "1";
+      return Number.parseInt(pageString);
+    },
+    displayJobs() {
+      const pageNumber = this.currentPage;
+      const firstJobIndex = (pageNumber - 1) * this.limit;
+      const lastJobIndex = pageNumber * this.limit;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = this.totalPages;
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+  },
   methods: {
     async getData() {
       const response = await axios.get("http://localhost:3000/jobs");
       this.jobs = response.data;
+      this.totalPages = Math.ceil(response.data.length / this.limit);
     },
   },
 };
