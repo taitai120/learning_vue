e67cb42295cd239370eed713619e765e805f2578
taitai120 <phantuantai7293@gmail.com>
2023-04-13 15:19:35 +0700

    finish 22

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,13 +2,54 @@
   <div
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
-    JobFiltersSidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="text-base my-4 font-semibold">What do you want do to?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+      <accordion header="Job Types">
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="w-1/2 h-8">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Between Vue and Me" type="checkbox" class="mr-3" />
+                <label for="Between Vue and Me">Between Vue</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
+                <label for="Vue and a Half Men">Vue and a Half Men</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </accordion>
+      <accordion header="???"></accordion>
+      <job-filters-sidebar-organizations />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import Accordion from "@/components/Shared/Accordion.vue";
+import JobFiltersSidebarOrganizations from "./JobFiltersSidebarOrganizations.vue";
 export default {
   name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+    Accordion,
+    JobFiltersSidebarOrganizations,
+  },
   mounted() {
     console.log("current logged in state: ", this.$store.state.isLoggedIn);
     this.$store.commit("LOGIN_USER");
--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from "@/App.vue";
 import "./assets/tailwind.css";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faAngleDown,
+  faAngleUp,
+} from "@fortawesome/free-solid-svg-icons";
 import router from "./router";
 import store from "@/store";
 
-library.add(faSearch);
+// library.add(faSearch);
+library.add([faSearch, faAngleUp, faAngleDown]);
 
 const app = createApp(App).use(store);
 
