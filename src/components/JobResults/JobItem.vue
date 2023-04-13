89093f685a696cd9dbec71ec169bbd290ee1beed
taitai120 <phantuantai7293@gmail.com>
2023-04-13 10:33:27 +0700

    finish 18

--- src/components/JobResults/JobItem.vue
+++ src/components/JobResults/JobItem.vue
@@ -1,19 +1,27 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-solid border-brand-gray-1 rounded hover:shawdow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
-        <h2 class="mb-2 text-2xl">Technical Program Manager</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
 
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bobo</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>Saigon</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ job.location }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -23,18 +31,17 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bachelor's degree or equivalent practice experience.</li>
-              <li>5 years of experience in program management.</li>
-              <li>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Perferendis hic enim inventore error quo, impedit doloribus! Aut
-                natus repellat optio.
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
 
           <div class="mt-2 text-center">
-            <router-link to="/jobs/results/1" class="text-brand-blue-1">
+            <router-link :to="jobPageLink" class="text-brand-blue-1">
               Expand
             </router-link>
           </div>
@@ -47,5 +54,19 @@
 <script>
 export default {
   name: "JobItem",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    console.log(this.job);
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,22 +1,32 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <job-item />
-      <job-item />
-      <job-item />
-      <job-item />
-      <job-item />
-      <job-item />
+      <job-item v-for="job in jobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 import JobItem from "@/components/JobResults/JobItem.vue";
 export default {
   name: "JobListing",
   components: {
     JobItem,
+  },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      const response = await axios.get("http://localhost:3000/jobs");
+      this.jobs = response.data;
+    },
   },
 };
 </script>
